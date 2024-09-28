8ecfce4e7267bde23acdd4af36b519f5ada18811
DavidC <d@dchere.com>
2024-09-28 14:23:56 +0200

    presunuti

--- webapp/src/main.ts
+++ webapp/src/main.ts
@@ -13,9 +13,25 @@ const notifier = (pap?: Paper) => {
 
 let initValue = 'PS:8,16|TR:2,6,1;2,7,1;2,8,1;2,9,1;2,10,1;3,5,2;3,6,2;3,7,2;3,8,1;3,9,1;3,10,1;4,5,2;4,6,2;4,7,2;4,8,1;4,10,1;5,6,1;5,7,1;5,8,1;5,9,1;5,10,1'
 
-if (svgel && serel) {
-  setupSVG(svgel as HTMLElement, initValue, notifier)
+export let differentInitValue = 'PS:8,8|TR:3,1,2;3,2,2;3,3,2;4,1,2;4,2,2;4,3,2'
+
+let currentBoard = initValue
+
+export function setit() {
+  if (currentBoard === initValue) {
+    currentBoard = differentInitValue
+  } else {
+    currentBoard = initValue
+  }
+  if (svgel && serel) {
+    svgel.innerHTML = ''
+    setupSVG(svgel as HTMLElement, currentBoard, notifier)
+  }
 }
+
+// if (svgel && serel) {
+//   setupSVG(svgel as HTMLElement, initValue, notifier)
+// }
 
 // let hashval = encodeURI(initValue)
 
